9537814295d19cdd10685f025bf4af336f061f1c
EC2 Default User <ec2-user@ip-172-31-25-180.us-east-2.compute.internal>
2017-11-05 00:43:35 +0000

    Updated for general AWS gridlabd setup

--- aws-gridlabd-setup.sh
+++ aws-gridlabd-setup.sh
@@ -1,91 +1,71 @@
 #!/bin/bash
 # 
-# This script sets up an AWS host for GridLAB-D running the IEEE123 feeder model in realtime
+# This script sets up an AWS host for GridLAB-D 
 #
 
-#elevate privileges to su
-sudo su
-# yum install wget -y  # install wget for redhat 7 on EC2
+# root check
+if [ $(whoami) != "root" ]; then
+	echo "must be root to run this script"
+	exit
+fi
 
-# yum update install git, php, apache, cmake, dev tools
-#wget https://s3-us-west-1.amazonaws.com/vader-lab/gridlabd-dependencies/install-base.sh
-#sh install-base.sh
+# system update
 yum update -y
-
-#install base software
 yum install git -y
 yum install httpd -y
 yum install php -y
-yum install cmake -y #this is a requirement for armadillo - linear algebra library
+yum install cmake -y 
 yum groupinstall "Development Tools" -y
-
-# MySQL Install server, connector, and libs
 yum install https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm -y
 yum install mysql56-server -y # for Amazon linux
-
-#install MySql Connector
-wget https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
-tar xf mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
-#rm mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
-cd mysql-connector-c-6.1.9-linux-glibc2.5-x86_64/
-cp bin/* /usr/local/bin
-cp -R include/* /usr/local/include
-cp -R lib/* /usr/local/lib
+if [ ! -d mysql-connector-c-6.1.9-linux-glibc2.5-x86_64 ]; then
+	wget https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
+	tar xf mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
+	rm mysql-connector-c-6.1.9-linux-glibc2.5-x86_64.tar.gz
+	cd mysql-connector-c-6.1.9-linux-glibc2.5-x86_64/
+	cp -u bin/* /usr/local/bin
+	cp -Ru include/* /usr/local/include
+	cp -Ru lib/* /usr/local/lib
+fi
 yum install mysql-libs -y
 
-#test MySQL Service
-#service mysqld start
-#service mysqld stop
-
-#yum install mysql -y #remove this later
-#copy gridlab source and install lib-xercesc
-#wget https://s3-us-west-1.amazonaws.com/vader-lab/gridlabd-dependencies/install-libxercesc.sh
-#sh install-libxercesc.sh
-#copy gridlab source and install lib-xercesc
 cd /usr/local/src
-mkdir gridlabd
-cd gridlabd
-git clone https://github.com/dchassin/gridlab-d .
-cd third_party
-. install_xercesc # need to source the script for automated run.
-
-#install armadillo - C++ linear algebra library
-cd /usr/local/src
-wget http://sourceforge.net/projects/arma/files/armadillo-7.800.1.tar.xz
-tar xf armadillo-7.800.1.tar.xz
-rm -f armadillo-7.800.1.tar.xz
-cd armadillo-7.800.1
-cmake .
-make install
-
-# clone IEEE123 model in www folder
-cd /var/www/html
-git clone https://github.com/dchassin/ieee123-aws
-cp -R ieee123-aws/* .
-rm -rf ieee123-aws/
-mkdir data output
-chmod -R 777 data output config
-chown -R apache.apache .
-
-#install gridlabd
-cd /home/ec2-user/gridlabd/source
+if [ ! -d gridlabd ]; then
+	git clone https://github.com/dchassin/gridlabd gridlabd
+	# install xercesc
+	(cd gridlabd/third_party; . install_xercesc)
+	# install armadillo
+	cd /usr/local/src
+	wget http://sourceforge.net/projects/arma/files/armadillo-7.800.1.tar.xz
+	tar xf armadillo-7.800.1.tar.xz
+	rm -f armadillo-7.800.1.tar.xz
+	cd armadillo-7.800.1
+	cmake .
+	make install
+else
+	cd gridlabd
+	git pull origin
+fi
+cd /usr/local/src/gridlabd
 autoreconf -isf
 ./customize configure
 make install
-export PATH=/usr/local/bin:$PATH
-gridlabd --validate
 
 # edit httpd.conf file and add index.php
-sed -e 's/DirectoryIndex .*/& index.php/' /etc/httpd/conf/httpd.conf >/tmp/httpd.conf
-mv /tmp/httpd.conf /etc/httpd/conf/httpd.conf
+if [ ! -f /etc/httpd/conf/httpd.original ]; then
+	cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.original
+	sed -e 's/DirectoryIndex .*/& index.php/' /etc/httpd/conf/httpd.original >/etc/httpd/conf/httpd.conf
+fi
 
 # install my web sql
 # cd var/www/html
 # wget http://downloads.sourceforge.net/project/mywebsql/stable/mywebsql-3.4.zip
 
 #### this  needs to happen last ####
-sed -e 's/^pored -e 's/^port=.*/#&/g;s/^socket=.*/#&/g;s/^datadir=.*/#&/g' /etc/my.cnf >/tmp/my.cnf
-mv /tmp/my.cnf /etc/my.cnf
+if [ ! -f /etc/my.original ]; then
+	cp /etc/my.cnf /etc/my.original
+	sed -e 's/^port=.*/#&/g;s/^socket=.*/#&/g;s/^datadir=.*/#&/g' /etc/my.original >/etc/my.cnf
+fi
 
 service mysqld restart
 # Create user gridlabd_ro and gridlabd in mysql database
@@ -101,4 +81,8 @@ END
 
 #start apache service
 service httpd start
-systemctl start httpd # on RHEL 7 only
+
+if [ ! -f /etc/bashrc.original ]; then
+	cp /etc/bashrc /etc/bashrc.original
+	echo 'export LD_LIBRARY_PATH=/usr/local/lib' >> /etc/bashrc
+fi
